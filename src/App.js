72d5193b78f836eb9689492a4897ac43c808fb2a
Cuitlazotlac <cuitlazotlac@gmail.com>
2022-02-12 18:01:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Footer from "./components/Footer";
+import NavMenu from "./components/NavMenu";
+import ScrollToTop from "./components/ScrollToTop";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavMenu />
+        <ScrollToTop />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   );
 }
-
-export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,180 @@
+import React from "react";
+import styled from "styled-components";
+import PText from "../components/PText";
+import Button from "../components/Button";
+// import AboutImg from '../assets/images/about-page-img.png';
+import AboutInfoItem from "../components/AboutInfoItem";
+import ContactBanner from "../components/ContactBanner";
+
+const AboutPageStyles = styled.div`
+  padding: 20rem 0 10rem 0;
+  .top-section {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+  }
+  .left {
+    flex: 3;
+  }
+  .right {
+    flex: 2;
+  }
+  .about__subheading {
+    font-size: 2.2rem;
+    margin-bottom: 2rem;
+    span {
+      background-color: var(--deep-dark);
+      padding: 0.5rem;
+      border-radius: 8px;
+    }
+  }
+  .about__heading {
+    font-size: 3.6rem;
+    margin-bottom: 3rem;
+  }
+  .about__info {
+    margin-bottom: 4rem;
+    .para {
+      max-width: 100%;
+    }
+  }
+  .right {
+    img {
+      border: 2px solid var(--gray-1);
+    }
+  }
+  .about__info__items {
+    margin-top: 15rem;
+  }
+  .about__info__item {
+    margin-bottom: 10rem;
+  }
+  .about__info__heading {
+    font-size: 3.6rem;
+    text-transform: uppercase;
+  }
+  @media only screen and (max-width: 768px) {
+    padding: 10rem 0;
+    .top-section {
+      flex-direction: column;
+      gap: 5rem;
+    }
+    .about__subheading {
+      font-size: 1.8rem;
+    }
+    .about__heading {
+      font-size: 2.8rem;
+    }
+    .about__info__heading {
+      font-size: 3rem;
+    }
+  }
+`;
+
+export default function About() {
+  return (
+    <>
+      <AboutPageStyles>
+        <div className="container">
+          <div className="top-section">
+            <div className="left">
+              <p className="about__subheading">
+                Hi, I am <span>Hayssem Elsayed</span>
+              </p>
+              <h2 className="about__heading">A freelance Web developer</h2>
+              <div className="about__info">
+                <PText>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, and more recently with desktop publishing software
+                  like Aldus PageMaker including versions of Lorem Ipsum.
+                  <br /> <br />
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum."
+                  <br />
+                  <br />
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum."
+                </PText>
+              </div>
+              <Button btnText="Download CV" btnLink="#" />
+            </div>
+            <div className="right">
+              <img src={"AboutImg"} alt="me" />
+            </div>
+          </div>
+          <div className="about__info__items">
+            <div className="about__info__item">
+              <h1 className="about__info__heading">Education</h1>
+
+              <AboutInfoItem
+                title="School"
+                items={["Nasirabad Govt. High School, Chattogram"]}
+              />
+              <AboutInfoItem
+                title="Collage"
+                items={["BAF Shaheen College Chattogram"]}
+              />
+              <AboutInfoItem
+                title="Varsity"
+                items={["University Of Chitiagong"]}
+              />
+            </div>
+            <div className="about__info__item">
+              <h1 className="about__info__heading">My Skills</h1>
+
+              <AboutInfoItem
+                title="FrontEnd"
+                items={["HTML", "CSS", "JavaScript", "REACT"]}
+              />
+              <AboutInfoItem
+                title="BackEnd"
+                items={["Node", "Express", "PHP"]}
+              />
+              <AboutInfoItem
+                title="Design"
+                items={["Photoshop", "After Effects", "Figma"]}
+              />
+            </div>
+            <div className="about__info__item">
+              <h1 className="about__info__heading">Experiences</h1>
+
+              <AboutInfoItem
+                title="2010-2012"
+                items={["junior developer at web Cifar"]}
+              />
+              <AboutInfoItem
+                title="2012-2016"
+                items={["Front end developer at web Cifar "]}
+              />
+              <AboutInfoItem
+                title="2016-"
+                items={["Freelance web Developer"]}
+              />
+            </div>
+          </div>
+        </div>
+        <ContactBanner />
+      </AboutPageStyles>
+    </>
+  );
+}
