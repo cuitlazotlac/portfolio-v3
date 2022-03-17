64367adcb41f073ac4c71a99dd2c75c61b149731
cuitlazotlac <cuitlazotlac@gmail.com>
2022-03-16 22:25:04 -0400

    focus on about section

--- src/components/AboutInfoItem.jsx
+++ src/components/AboutInfoItem.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
-import PText from './PText';
+import React from "react";
+import styled from "styled-components";
+import PText from "./PText";
 
 const AboutItemStyles = styled.div`
   display: flex;
@@ -19,7 +19,7 @@ const AboutItemStyles = styled.div`
     left: 18rem;
   }
   .item {
-    background-color: var(--deep-dark);
+    background-color: var(--secondary-color);
     padding: 1rem;
     border-radius: 8px;
   }
@@ -38,15 +38,15 @@ const AboutItemStyles = styled.div`
 `;
 
 export default function AboutInfoItem({
-  title = 'Title',
-  items = ['HTML', 'CSS'],
+  title = "Title",
+  items = ["HTML", "CSS"],
 }) {
   return (
     <AboutItemStyles>
-      <h1 className='title'>{title}</h1>
-      <div className='items'>
+      <h1 className="title">{title}</h1>
+      <div className="items">
         {items.map((item, index) => (
-          <div className='item' key={index}>
+          <div className="item" key={index}>
             <PText>{item}</PText>
           </div>
         ))}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,72 +6,6 @@ import Button from "../components/Button";
 import AboutInfoItem from "../components/AboutInfoItem";
 import ContactBanner from "../components/ContactBanner";
 
-const AboutPageStyles = styled.div`
-  padding: 20rem 0 10rem 0;
-  .top-section {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 2rem;
-  }
-  .left {
-    flex: 3;
-  }
-  .right {
-    flex: 2;
-  }
-  .about__subheading {
-    font-size: 2.2rem;
-    margin-bottom: 2rem;
-    span {
-      background-color: var(--deep-dark);
-      padding: 0.5rem;
-      border-radius: 8px;
-    }
-  }
-  .about__heading {
-    font-size: 3.6rem;
-    margin-bottom: 3rem;
-  }
-  .about__info {
-    margin-bottom: 4rem;
-    .para {
-      max-width: 100%;
-    }
-  }
-  .right {
-    img {
-      border: 2px solid var(--font-color);
-    }
-  }
-  .about__info__items {
-    margin-top: 15rem;
-  }
-  .about__info__item {
-    margin-bottom: 10rem;
-  }
-  .about__info__heading {
-    font-size: 3.6rem;
-    text-transform: uppercase;
-  }
-  @media only screen and (max-width: 768px) {
-    padding: 10rem 0;
-    .top-section {
-      flex-direction: column;
-      gap: 5rem;
-    }
-    .about__subheading {
-      font-size: 1.8rem;
-    }
-    .about__heading {
-      font-size: 2.8rem;
-    }
-    .about__info__heading {
-      font-size: 3rem;
-    }
-  }
-`;
-
 export default function About() {
   return (
     <>
@@ -178,3 +112,69 @@ export default function About() {
     </>
   );
 }
+
+const AboutPageStyles = styled.div`
+  padding: 20rem 0 10rem 0;
+  .top-section {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+  }
+  .left {
+    flex: 3;
+  }
+  .right {
+    flex: 2;
+  }
+  .about__subheading {
+    font-size: 2.2rem;
+    margin-bottom: 2rem;
+    span {
+      background-color: var(--secondary-color);
+      padding: 0.5rem;
+      border-radius: 8px;
+    }
+  }
+  .about__heading {
+    font-size: 3.6rem;
+    margin-bottom: 3rem;
+  }
+  .about__info {
+    margin-bottom: 4rem;
+    .para {
+      max-width: 100%;
+    }
+  }
+  .right {
+    img {
+      border: 2px solid var(--font-color);
+    }
+  }
+  .about__info__items {
+    margin-top: 15rem;
+  }
+  .about__info__item {
+    margin-bottom: 10rem;
+  }
+  .about__info__heading {
+    font-size: 3.6rem;
+    text-transform: uppercase;
+  }
+  @media only screen and (max-width: 768px) {
+    padding: 10rem 0;
+    .top-section {
+      flex-direction: column;
+      gap: 5rem;
+    }
+    .about__subheading {
+      font-size: 1.8rem;
+    }
+    .about__heading {
+      font-size: 2.8rem;
+    }
+    .about__info__heading {
+      font-size: 3rem;
+    }
+  }
+`;
